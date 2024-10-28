34de10d733ca1751947d54db59a999b068db8ea8
Shubham53Mishra <21cse53@vmkvec.edu.in>
2024-10-28 15:36:01 +0530

    add the style

--- ContactUs.php
+++ ContactUs.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Section</title>
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-    <!-- <style>
+    <style>
         * {
             margin: 0;
             padding: 0;
@@ -254,7 +254,7 @@
             margin: 0; /* Remove default margin */
             margin-right: 20px;
         }
-    </style> -->
+    </style> 
 </head>
 <body>
     <!-- Header Section -->
